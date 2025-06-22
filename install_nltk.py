e298b69473cfe62ddfbcc7feb1a74595b9f24c52
Shawn Mikel Hobayan <hobayan.shawnmikel@gmail.com>
2025-06-23 02:06:57 +0800

    install nltk for better paraphrasing and summarizing

---
+++ install_nltk.py
@@ -0,0 +1,5 @@
+import nltk
+
+nltk.download('punkt')       # For sentence and word tokenizing
+nltk.download('wordnet')     # For synonym lookup
+nltk.download('omw-1.4')     # Optional: word meaning references
